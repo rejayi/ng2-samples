7d671f13cd93fe3e7be10a0ad27761b5b3a5122a
Rejayi cs <rejayi.chandra2experionglobal.com>
2017-03-13 12:29:24 +0530

    Name change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>ng2-samples</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
